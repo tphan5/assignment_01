c0460e0aa32920bf5562c1e5c628529512d7dc49
Jay <jlaura@asu.edu>
2015-11-04 21:28:00 -0700

    Altered to add a failing test

--- tests.py
+++ tests.py
@@ -3,10 +3,28 @@ import unittest
 class Test_Assignment_01(unittest.TestCase):
     
     def setUp(self):
+        """
+        Code called before every other test is run.
+        """
         pass
     
-    def test_travis_running(self):
+    def test_pass(self):
+        """
+        A test that will pass.
+        """
         self.assertTrue(True)
+
+    def test_fail(self):
+        """
+        A test that will fail.
+        """
+        self.assertFalse(True)
+
+    def tearDown(self):
+        """
+        Code called after ever other test is run.
+        """
+        pass
 
 if __name__ == '__main__':
     unittest.main()
